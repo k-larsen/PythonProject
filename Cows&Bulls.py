31f91ea7775386bf4395f70d41f47b16bb13836d
k-larsen <ktl99548@bethel.edu>
2016-02-23 15:22:22 -0600

    Description
    Brief intro to the game

--- Cows&Bulls.py
+++ Cows&Bulls.py
@@ -1,8 +1,15 @@
 import random
 
+print "Welcome to Mastermind\n" \
+      "You're objective is to guess a 4 digit number\n" \
+      "For every number that is accurate to the hidden number a COW will be awarded,\n" \
+      "For every number that is in the sequence but not in the right place will be awarded with a BULL\n"\
+      "Guess until you get the number correct\nGood Luck"
+
 number = random.randint(1000, 9999) # generate random number with 4 digits
 
-
+def guessGame(number):
+    return number
 
 
 
