ac58a75fe0a841bbf1e614aefcfb27b87ecc52ce
k-larsen <ktl99548@bethel.edu>
2016-02-26 13:42:55 -0600

    SwitchBullCow
    When either a bull or cow is found, replace the index in the string
    with a ‘B’ or ‘C’ accordingly to prevent duplicates.

--- Cows&Bulls.py
+++ Cows&Bulls.py
@@ -6,7 +6,7 @@ print "Welcome to Mastermind\n" \
       "For every number that is in the sequence but not in the right place will be awarded with a BULL\n"\
       "Guess until you get the number correct\nGood Luck"
 
-number = random.randint(1000, 9999) # generate random number with 4 digits
+number = random.randint(0000, 9999) # generate random number with 4 digits
 
 
 # bulk of game --> actual guessing process
@@ -19,11 +19,13 @@ def guessGame(number):
         for i in range(4):
             if guess[i] == str(number)[i]:
                 cow+=1
+                number = str(number)[:i]+"C"+str(number)[j+1:]
             else:
-                for j in range(1,3):
+                for j in range(0,3):
                     if guess[i] == str(number)[j]:
                         bull+=1
-                        
+                        number = str(number)[:j]+"B"+str(number)[j+1:]
+
 
         # put number into strings to compare indices
         print "COW: "+str(cow)
