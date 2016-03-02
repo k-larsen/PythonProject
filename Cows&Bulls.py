c0bb6c4ccb4fbca2fd953234fecd66143da38f8b
k-larsen <ktl99548@bethel.edu>
2016-03-02 12:49:01 -0600

    Added Comments
    comments added

--- Cows&Bulls.py
+++ Cows&Bulls.py
@@ -1,42 +1,52 @@
 import random
 
+# game instructions
 print "Welcome to Mastermind\n" \
       "You're objective is to guess a 4 digit number\n" \
       "For every number that is accurate to the hidden number a COW will be awarded,\n" \
       "For every number that is in the sequence but not in the right place will be awarded with a BULL\n"\
       "Guess until you get the number correct\nGood Luck"
 
-number = random.randint(0000, 9999) # generate random number with 4 digits
-
+number = ""
+# concatenate four integers to make number
+for i in range(4):
+    number += str(random.randint(0,9))
+print number
 
 # bulk of game --> actual guessing process
 def guessGame(number):
     guess = raw_input("Enter a 4 digit number: ")
     editNumber = number
-    cow = 0
-    bull = 0
-    guessCount = 0
-    while str(number) != guess:
+    guessCount = 1
+    while str(number) != guess: # guess number doesn't equal random number
+        cow = 0
+        bull = 0
         for i in range(4):
-            if guess[i] == str(editNumber)[i]:
+            if guess[i] == str(editNumber)[i]:  #current match up is a cow
                 cow+=1
                 editNumber = str(editNumber)[:i]+"C"+str(editNumber)[i+1:] #replace number in string with C
-            else:
+            else: # check for possible bull with current number in guess number
                 for j in range(0,4):
-                    if guess[i] == str(editNumber)[j]:
+                    if guess[i] == str(editNumber)[j]: # current match is a bull
                         bull+=1
                         editNumber = str(editNumber)[:j]+"B"+str(editNumber)[j+1:] #replace number in string with B
                         break
 
 
-        # put number into strings to compare indices
-        print "COW: "+str(cow)
-        print "BULL: "+str(bull)
+        # put cows/bulls into strings to compare indices
+        print "COW: "+str(cow)+"\n"\
+                "BULL: "+str(bull)
 
 
-        guessCount+=1
+        guessCount+=1 # total of guesses increases
         guess = raw_input("Enter a 4 digit number")
-    print "You took "+str(guessCount)+" tries."
+        editNumber = number
+    print "You guessed correctly.\nTries Taken: "+str(guessCount)
 
-
+# play the game
 guessGame(number)
+
+
+
+""" TODO fix issue with input numbers != 4
+Delete print number at beginning """
